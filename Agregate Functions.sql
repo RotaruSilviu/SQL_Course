5ea39795e352ee1d9b33d27c38a055f0b1574ed2
RotaruSilviu <159648340+RotaruSilviu@users.noreply.github.com>
2025-02-12 22:44:16 +0200

    Update Agregate Functions.sql

--- Agregate Functions.sql
+++ Agregate Functions.sql
@@ -12,3 +12,84 @@ from books;
 select count(*)
 from books
 where title like '%the%'; -- numara cate titluri au the in titlu si afiseaza doar numarul lor, nu si titlurile cu count
+
+--  GROUP BY  //////////
+select author_lname
+from books
+group by author_lname; -- Grupeaza valorile distinctive.
+
+select author_lname, count(*) AS books_written
+from books
+group by author_lname
+order by books_written DESC;
+
+select released_year, count(*)
+from books
+GROUP BY released_year
+order by released_year;
+
+select author_lname , count(*)
+from books
+group by author_lname;
+
+-- MIN and MAX//////////////////
+
+select  min(released_year)
+from books;
+
+select max(pages)
+from books;
+
+select min(author_lname) -- frunctioneaza si pentru string, va gasi cea mai mica valoare alfabetica. 
+from books;
+
+-- Titlu celei mai lungi carti:
+
+select title, pages
+from books
+order by pages desc limit 1; -- aici ne limiteaza sa selkecteam doar o optiune, daca sunt mai multe randuri cu aceasi valoare nu se va afisa.
+
+select * 
+from books
+where pages = (select max(pages)
+				from books); -- acesta e un sequeries, am folosit un select in alt select deci e o subrutina a primului SELECT
+							-- aici daca avem mai multe randuri cu o valoare maxima  se vor afisa toate
+
+select title, released_year
+from books
+where released_year = (select min(released_year) 
+						from books);
+
+-- GROUP BY MULTIPLE COLUMNS ///////
+
+select author_fname, author_lname 
+from books;
+
+select author_fname, author_lname 
+from books
+order by author_lname;
+
+
+select author_lname, author_fname, count(*)
+from books
+GROUP BY author_lname, author_fname;
+-- sau asa
+select concat(author_fname, ' ' , author_lname) as Full_Name, count(*)
+from books
+GROUP BY Full_Name;
+
+-- MIN and MAX with GROUP BY
+
+select author_lname, author_fname, min(released_year) as first_book
+from books
+group by author_lname,author_fname
+order by first_book;
+
+select author_lname,
+		author_fname,
+		count(*) AS boks_written, 
+        max(released_year) AS latest_release, 
+        min(released_year) AS earliest_release,
+        max(pages) AS longest_page_count
+from books
+group by author_lname,author_fname;
