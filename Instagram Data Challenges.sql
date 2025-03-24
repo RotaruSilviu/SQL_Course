36dcaa601bc879dacdd5efca13629fc6f6cbfa36
RotaruSilviu <159648340+RotaruSilviu@users.noreply.github.com>
2025-03-24 22:49:22 +0200

    Section with fun challenges.

---
+++ Instagram Data Challenges.sql
@@ -0,0 +1,49 @@
+-- Working with lots of IG data;
+
+-- IG challenge nr 1
+
+ select *
+ from users
+ ORDER BY created_at limit 5;
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
