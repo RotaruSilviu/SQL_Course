053c1876475a0e67a2b8227ab5d6db0f487ad1d8
RotaruSilviu <159648340+RotaruSilviu@users.noreply.github.com>
2025-04-07 22:09:00 +0300

    Cointinuing with the web app

--- App/app.js
+++ App/app.js
@@ -5,18 +5,49 @@
 // // for (var i = 0; i < 500 ; i++){
 // //     // insert a new user into the databse
 // // }
+// faker.internet.email()
+// console.log(faker.internet.email(), faker.date.past())
+// console.log(faker.date.past().toString());
+
+// function generateAdress() {
+//     console.log(faker.location.streetAddress());
+//     console.log(faker.location.city());
+//     console.log(faker.location.state());
+// }
+
+// generateAdress()
+
 
 // npm install @faker-js/faker
+// npm install mysql2
+//mysql -u root -p
+// control c : ca sa iesi din prompt cand se blocheaza terminalul
+
 var { faker } = require('@faker-js/faker');
+var mysql = require('mysql2');
 
-// faker.internet.email()
-console.log(faker.internet.email(), faker.date.past())
-console.log(faker.date.past().toString());
+// login for mysql
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Daniela407#',
+  database: 'join_us'
+});
 
-function generateAdress() {
-    console.log(faker.location.streetAddress());
-    console.log(faker.location.city());
-    console.log(faker.location.state());
-}
+var q = 'select curdate()';
+connection.query(q, function (error, results, fields) {
+  if (error) throw error;
+  console.log(results);
+});
 
-generateAdress()
+connection.end(); // casa nu se blocheze terminalul
+
+//   console.log(results[0].time);
+//   console.log(results[0].date.toString());
+//   console.log(results[0].now.toString());
+
+
+
+
+
+
