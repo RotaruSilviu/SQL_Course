250c33e6f7c54733bc48a81301607a5223ef09c6
RotaruSilviu <159648340+RotaruSilviu@users.noreply.github.com>
2025-04-12 23:29:36 +0300

    Inserting 500 users in bulk

--- App/app.js
+++ App/app.js
@@ -24,16 +24,6 @@
 //mysql -u root -p
 // control c : ca sa iesi din prompt cand se blocheaza terminalul
 
-var { faker } = require('@faker-js/faker');
-var mysql = require('mysql2');
-
-// login for mysql
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Daniela407#',
-  database: 'join_us'
-});
 
 // var q = 'select curtime() as time, curdate() as date, now() as now';
 // connection.query(q, function (error, results, fields) {
@@ -57,9 +47,96 @@ const connection = mysql.createConnection({
 // });
 // connection.end();
 
-var q = 'SELECT count(*) as total from users';
-connection.query(q, function (error, results, fields) {
-  if (error) throw error;
-  console.log(results[0].total);
+// SELECTING DATA////////////////////////
+
+// var q = 'SELECT count(*) as total from users';
+// connection.query(q, function (error, results, fields) {
+//   if (error) throw error;
+//   console.log(results[0].total);
+// });
+// connection.end();
+
+// INSERTING DATA:///////////////////////////
+
+// var q = 'insert into users(email) values ("rusty_the_dog@gmail.com")';
+// connection.query(q, function (error, results, fields) {
+//   if (error) throw error;
+//   console.log(results);
+// });
+// connection.end();
+
+// INSERTING DATA 2 WITH FAKER//////////////////////////////
+
+// var person = {
+//     email: faker.internet.email(),
+//     created_at: faker.date.past()
+//  };
+
+//  console.log(person);
+// var end_result = connection.query('insert into users set ?', person, function(err, result){
+//   if (err) throw err;
+//   console.log(result);
+// });
+
+// console.log(end_result.sql);
+// connection.end();
+
+// console.log(faker.date.);
+
+
+// INSERTING LOTS OF DATA////////////////////////////////////////////////////////
+
+// var data = [];
+// for (var i = 0; i < 500; i++){
+//   data.push([
+//     faker.internet.email(),
+//     faker.date.past()
+//     ]);
+// }
+
+// // console.log(data);
+
+// var q = 'INSERT INTO users (email,created_at) VALUES ?';
+
+// connection.query(q, [data], function(err, result) {
+//     console.log(err);
+//     console.log(result);
+// });
+
+// connection.end();
+
+// CLEAN CODE FOR INSERTING 500 USERS IN BULK
+
+var { faker } = require('@faker-js/faker');
+var mysql = require('mysql2');
+
+// login for mysql
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Daniela407#',
+  database: 'join_us'
 });
+
+
+var data = [];
+for (var i = 0; i < 500; i++){
+  data.push([
+    faker.internet.email(),
+    faker.date.past()
+    ]);
+}
+
+var q = 'INSERT INTO users (email,created_at) VALUES ?';
+connection.query(q, [data], function(err, result) {
+    console.log(err);
+    console.log(result);
+});
+
 connection.end();
+
+
+
+
+
+
--- App/schema.sql
+++ App/schema.sql
@@ -1,7 +1,60 @@
-create table users(
-    email VARCHAR(255) PRIMARY KEY,
-    created_at TIMESTAMP DEFAULT NOW()
-);
+-- create table users(
+--     email VARCHAR(255) PRIMARY KEY,
+--     created_at TIMESTAMP DEFAULT NOW()
+-- );
 
-insert into users(email) values
-("Katie34@yahoo.com"),("Tunde@gmail.com");
+-- insert into users(email) values
+-- ("Katie34@yahoo.com"),("Tunde@gmail.com");
+
+-- insert into users(email, created_at) VALUES
+-- ('dusty@gmail.com', 'Wed Aug 21 2024 17:06:23 GMT+0300');
+
+select * from users
+order by created_at desc;
+select version();
+select now();
+
+-- to run a file in vs code use the terminal and type "source name_of_the_file.sql" !!!!!!!!
+
+
+-- EXERCISES ////////
+select DATE_FORMAT(created_at, '%M %D %Y')  as earliest_date
+from users
+order by created_at
+limit 1;
+
+select * 
+from users
+ORDER BY created_at
+limit 1;
+
+select 
+    MONTHNAME(created_at) as MONTH,
+    count(*) as count
+from users
+group by MONTH
+ORDER BY count desc;
+
+select count(*) as yahoo_users
+from users
+where email like ('%@yahoo.com');
+
+
+select 
+    SUBSTRING_INDEX(email, '@', -1) as provider,
+    count(*) as provider_count
+from users
+GROUP BY provider
+order by provider_count desc;
+
+SELECT 
+    CASE 
+        when email like ('%@yahoo.com') then 'yahoo'
+        when email like ('%@gmail.com') then 'gmail'
+        when email like ('%@hotmail.com') then 'hotmail'
+        else 'other' 
+    END as provider,
+    count(*) as total_users
+from users
+GROUP BY provider
+order by total_users desc;
